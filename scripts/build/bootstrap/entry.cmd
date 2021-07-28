b91940ce63979718249c938565708cbb141801ed
shiranj <shiranj@amazon.com>
2021-07-28 00:15:17 -0700

    Add LY_CMAKE_PATH env

--- scripts/build/bootstrap/entry.cmd
+++ scripts/build/bootstrap/entry.cmd
@@ -1,4 +1,5 @@
 cd %CODEBUILD_SRC_DIR%
-set PATH="%PATH%;C:\Program Files\CMake\bin"
+set LY_CMAKE_PATH="C:\Program Files\CMake\bin"
 python/get_python.bat
+set PATH="%PATH%;C:\Program Files\CMake\bin"
 python/python.cmd -u scripts\build\ci_build.py --platform Windows --type profile_vs2019
