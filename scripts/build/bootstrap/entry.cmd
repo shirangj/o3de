c1ace521dc147ea47d79f1e34188f2ab88b51db4
shiranj <shiranj@amazon.com>
2021-07-28 00:29:36 -0700

    Add entry script

--- scripts/build/bootstrap/entry.cmd
+++ scripts/build/bootstrap/entry.cmd
@@ -1,5 +1,4 @@
 cd %CODEBUILD_SRC_DIR%
-set LY_CMAKE_PATH="C:\Program Files\CMake\bin"
+set "PATH=%PATH%;C:\Program Files\CMake\bin"
 python/get_python.bat
-set PATH="%PATH%;C:\Program Files\CMake\bin"
 python/python.cmd -u scripts\build\ci_build.py --platform Windows --type profile_vs2019
