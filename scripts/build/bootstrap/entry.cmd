42252d4c5b4d6246b8a8bf1c4bd601ded94711a0
shiranj <shiranj@amazon.com>
2021-07-28 01:41:58 -0700

    Update entry.cmd

--- scripts/build/bootstrap/entry.cmd
+++ scripts/build/bootstrap/entry.cmd
@@ -1,4 +1,8 @@
 cd %CODEBUILD_SRC_DIR%
-set "PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin"
+
+powershell.exe scripts/build/build_node/Platform/Windows/install_vsbuildtools.ps1
+choco install cmake -y --installargs 'ADD_CMAKE_TO_PATH=System'
+
+rem set "PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin"
 call python/get_python.bat
 call python/python.cmd -u scripts\build\ci_build.py --platform Windows --type profile_vs2019
