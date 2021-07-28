4d6f2403399f18bc27968be5be6880b417b9ce11
shiranj <shiranj@amazon.com>
2021-07-28 14:41:11 -0700

    update entry script

--- scripts/build/bootstrap/entry.cmd
+++ scripts/build/bootstrap/entry.cmd
@@ -5,4 +5,4 @@ pwsh scripts/build/build_node/Platform/Windows/install_utiltools.ps1
 set "PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin"
 call python/get_python.bat
 call python/python.cmd -u scripts\build\ci_build.py --platform Windows --type profile_vs2019
-type "build/windows_vs2019/CMakeFiles/CMakeOutput.log"
+type ".\build\windows_vs2019\CMakeFiles\CMakeOutput.log"
