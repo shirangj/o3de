864c207fcdcddb0e32a8dcf658c4dc1a0c6069a5
shiranj <shiranj@amazon.com>
2021-07-28 01:25:52 -0700

    Add vs2019 to PATH

--- scripts/build/bootstrap/entry.cmd
+++ scripts/build/bootstrap/entry.cmd
@@ -1,4 +1,4 @@
 cd %CODEBUILD_SRC_DIR%
-set "PATH=%PATH%;C:\Program Files\CMake\bin"
+set "PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin"
 call python/get_python.bat
 call python/python.cmd -u scripts\build\ci_build.py --platform Windows --type profile_vs2019
