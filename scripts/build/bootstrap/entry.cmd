74bbc9cdc979fef50758064135d16a174f7c66c3
shiranj <shiranj@amazon.com>
2021-07-28 01:09:07 -0700

    Fix bat call

--- scripts/build/bootstrap/entry.cmd
+++ scripts/build/bootstrap/entry.cmd
@@ -1,4 +1,4 @@
 cd %CODEBUILD_SRC_DIR%
 set "PATH=%PATH%;C:\Program Files\CMake\bin"
-python/get_python.bat
+call python/get_python.bat
 call python/python.cmd -u scripts\build\ci_build.py --platform Windows --type profile_vs2019
