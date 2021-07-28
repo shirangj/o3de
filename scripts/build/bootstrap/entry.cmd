3d54e69755517c1af8a4cc43f7593d363045650f
shiranj <shiranj@amazon.com>
2021-07-28 13:26:13 -0700

    update powershell call

--- scripts/build/bootstrap/entry.cmd
+++ scripts/build/bootstrap/entry.cmd
@@ -1,8 +1,7 @@
 cd %CODEBUILD_SRC_DIR%
 
-powershell.exe scripts/build/build_node/Platform/Windows/install_vsbuildtools.ps1
-powershell.exe scripts/build/build_node/Platform/Windows/install_utiltools.ps1
-
+pwsh.exe scripts/build/build_node/Platform/Windows/install_vsbuildtools.ps1
+pwsh.exe scripts/build/build_node/Platform/Windows/install_utiltools.ps1
 set "PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin"
 call python/get_python.bat
 call python/python.cmd -u scripts\build\ci_build.py --platform Windows --type profile_vs2019
