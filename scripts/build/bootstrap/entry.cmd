36b2e01dd88a4db176c454ad437ebdd8d5ae348c
shiranj <shiranj@amazon.com>
2021-07-28 14:05:17 -0700

    update entry script

--- scripts/build/bootstrap/entry.cmd
+++ scripts/build/bootstrap/entry.cmd
@@ -1,7 +1,8 @@
 cd %CODEBUILD_SRC_DIR%
 
-pwsh.exe scripts/build/build_node/Platform/Windows/install_vsbuildtools.ps1
-pwsh.exe scripts/build/build_node/Platform/Windows/install_utiltools.ps1
+pwsh scripts/build/build_node/Platform/Windows/install_vsbuildtools.ps1
+pwsh scripts/build/build_node/Platform/Windows/install_utiltools.ps1
 set "PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin"
 call python/get_python.bat
 call python/python.cmd -u scripts\build\ci_build.py --platform Windows --type profile_vs2019
+type build/windows_vs2019/CMakeFiles/CMakeOutput.log
